35c53aaf40a991b9e826a78d791183ba6b1d10b8
Juanmunoz8 <131785486+Juanmunoz8@users.noreply.github.com>
2025-02-27 11:45:32 -0600

    Update Simpy.py

--- src/Simpy.py
+++ src/Simpy.py
@@ -3,6 +3,28 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+RANDOM_SEED = 42
+INTERVALS = [10, 5, 1]
+PROCESS_COUNTS = [25, 50, 100, 150, 200]
+MEMORY_CAPACITY = [100, 200]  # Se probarán ambas capacidades
+CPU_SPEEDS = [3, 6]  # Se probarán ambas velocidades
+CPU_COUNTS = [1, 2]  # Se probarán 1 y 2 CPUs
+
+def proceso(env, name, ram, cpu, instrucciones, resultados):
+    llegada = env.now
+    memoria = random.randint(1, 10)
+    
+    yield ram.get(memoria)
+    
+    with cpu.request() as req:
+        yield req
+        while instrucciones > 0:
+            yield env.timeout(1)
+            instrucciones -= min(instrucciones, CPU_SPEED)
+    
+    ram.put(memoria)
+    resultados.append(env.now - llegada)
+
 
 def generar_procesos(env, interval, num_procesos, ram, cpu, resultados):
     for i in range(num_procesos):
